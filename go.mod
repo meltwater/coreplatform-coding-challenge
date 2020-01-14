ac6bbb9fa5972ceb07fe0f4dbb02bff66539f6e3
Ollie Parsley <ollie@ollieparsley.com>
2020-01-14 17:44:20 +0000

    Basic skeleton

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/meltwater/coreplatform-coding-challenge
 
 go 1.13
 
-require github.com/Jeffail/benthos/v3 v3.7.0
+require (
+	github.com/Jeffail/benthos v1.20.4
+	github.com/Jeffail/benthos/v3 v3.7.0
+)
--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"time"
+
 	"github.com/Jeffail/benthos/v3/lib/log"
 	"github.com/Jeffail/benthos/v3/lib/metrics"
 	"github.com/Jeffail/benthos/v3/lib/processor"
 	"github.com/Jeffail/benthos/v3/lib/service"
 	"github.com/Jeffail/benthos/v3/lib/types"
-	"github.com/meltwater/coreplatform-coding-challenge/lib/challenge"
 )
 
 func main() {
 	processor.RegisterPlugin(
 		"coding_challenge",
 		func() interface{} {
-			s := challenge.ChallengePlugin{}
+			s := ChallengePlugin{}
 			return &s
 		},
 		func(
@@ -22,9 +23,51 @@ func main() {
 			logger log.Modular,
 			stats metrics.Type,
 		) (types.Processor, error) {
-			return iconf.(*challenge.ChallengePlugin), nil
+			return iconf.(*ChallengePlugin), nil
 		},
 	)
 
 	service.Run()
+}
+
+//
+// ChallengePlugin
+//
+
+// ChallengePlugin ...
+type ChallengePlugin struct{}
+
+// ProcessMessage returns messages mutated with their sarcasm level.
+func (c *ChallengePlugin) ProcessMessage(msg types.Message) ([]types.Message, types.Response) {
+	newMsg := msg.Copy()
+
+	newMsg.Iter(func(i int, p types.Part) error {
+		// Call DoSomething and store the result as the full content of the part
+		result := c.DoSomething(p.Get())
+		p.Set(result)
+		return nil
+	})
+
+	return []types.Message{newMsg}, nil
+}
+
+// CloseAsync does nothing.
+func (c *ChallengePlugin) CloseAsync() {}
+
+// WaitForClose does nothing.
+func (c *ChallengePlugin) WaitForClose(timeout time.Duration) error {
+	return nil
+}
+
+// DoSomething ...
+func (c *ChallengePlugin) DoSomething(data []byte) []byte {
+	//
+	//
+	//
+	// This is where you will implement the plugin
+	//
+	//
+	//
+
+	return data
 }
